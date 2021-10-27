f3eb28021a2b88c416738f6389d210b1bc196ba5
kovsonq <kovsonq@gmail.com>
2021-10-27 16:55:19 +0300

    Fifth chapter.

---
+++ src/main/java/course/functional/chapterOption/A.java
@@ -0,0 +1,19 @@
+package course.functional.chapterOption;
+
+public class A {
+    public A(int x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int x;
+    double y;
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
---
+++ src/main/java/course/functional/chapterOption/Car.java
@@ -0,0 +1,19 @@
+package course.functional.chapterOption;
+
+public class Car {
+    String name;
+    double price;
+
+    public Car(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
